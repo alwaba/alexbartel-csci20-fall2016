cfb604e317aeebec627e88af50fc3044f5690146
Alex Bartel <abartel001@student.butte.edu>
2016-08-25 21:06:20 +0000

    alex bartel lab1

---
+++ lab1/lab1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+   cout << "      *           *          " << endl;
+   cout << "    * * *       * * *        " << endl;
+   cout << "   *     **   **     *       " << endl;
+   cout << "  **       ***       **      " << endl;
+   cout << "   *     **   **     *       " << endl;
+   cout << "    * * *       * * *        " << endl;
+   cout << "      *           *          " << endl;
+
+   return 0;
+}
+git add lab1
+git commit –a –m ‘Alex Bartel lab1’
+git push
